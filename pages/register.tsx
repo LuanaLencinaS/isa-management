b3fac1a4520126be7b38c96876462ffe80246c41
Luana Lencina <analulencina@hotmail.com>
2023-04-04 23:29:01 -0300

    :art: feat(style): implementa styles + form exemplo

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,4 +1,6 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import * as Tabs from "@radix-ui/react-tabs";
+import styledSign from "@/styles/Sign.module.css";
 
 enum GenderEnum {
   female = "female",
@@ -26,35 +28,118 @@ export default function Register() {
   };
 
   return (
-    <div>
+    <div className={"page"}>
       <h1>Registro Page</h1>
       <p>Learn more Register us!</p>
 
-      <form>
-        {/* <form onSubmit={handleSubmit(onSubmit)}> */}
-        <label>First Name</label>
-        <input
-          {...register("firstName", {
-            required: true,
-            maxLength: 20,
-            pattern: /^[A-Za-z]+$/i,
-          })}
-          aria-invalid={errors.firstName ? "true" : "false"}
-        />
-        {errors.firstName?.type === "required" && (
-          <p role="alert">First name is required</p>
-        )}
+      <Tabs.Root
+        className={[
+          "TabsRoot",
+          styledSign.card,
+          styledSign["shadow-primary"],
+        ].join(" ")}
+        defaultValue="tab1"
+      >
+        <Tabs.List className="TabsList" aria-label="Manage your account">
+          <Tabs.Trigger className="TabsTrigger" value="tab1">
+            Cadastre-se
+          </Tabs.Trigger>
+          <Tabs.Trigger className="TabsTrigger" value="tab2">
+            Login
+          </Tabs.Trigger>
+        </Tabs.List>
 
-        <label>Gender Selection</label>
-        <select {...register("gender")}>
-          <option value="female">Feminino</option>
-          <option value="male">Masculino</option>
-          <option value="other">Outro</option>
-        </select>
+        <Tabs.Content className="TabsContent" value="tab1">
+          <p className="Text">
+            Make changes to your account here. Click save when youre done.
+          </p>
 
-        {/* <input type="submit" /> */}
-        <button onClick={handleSubmit(onSubmit)}>Enviar</button>
-      </form>
+          <form>
+            {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+            <fieldset className="Fieldset">
+              <label className="Label" htmlFor="firstName">
+                First Name
+              </label>
+              <input
+                className="Input"
+                id="firstName"
+                type="text"
+                {...register("firstName", {
+                  required: true,
+                  maxLength: 20,
+                  pattern: /^[A-Za-z]+$/i,
+                })}
+                aria-invalid={errors.firstName ? "true" : "false"}
+              />
+              {errors.firstName?.type === "required" && (
+                <p role="alert">First name is required</p>
+              )}
+            </fieldset>
+
+            <fieldset className="Fieldset">
+              <label className="Label" htmlFor="gender">
+                Gender Selection
+              </label>
+              <select id="gender" {...register("gender")}>
+                <option value="female">Feminino</option>
+                <option value="male">Masculino</option>
+                <option value="other">Outro</option>
+              </select>
+            </fieldset>
+            <label></label>
+
+            {/* <input type="submit" /> */}
+            <div
+              style={{
+                display: "flex",
+                marginTop: 20,
+                justifyContent: "flex-end",
+              }}
+            >
+              <button className="Button green" onClick={handleSubmit(onSubmit)}>
+                Enviar
+              </button>
+            </div>
+          </form>
+        </Tabs.Content>
+
+        <Tabs.Content className="TabsContent" value="tab2">
+          <p className="Text">
+            Change your password here. After saving, be logged out.
+          </p>
+
+          <fieldset className="Fieldset">
+            <label className="Label" htmlFor="currentPassword">
+              Current password
+            </label>
+            <input className="Input" id="currentPassword" type="password" />
+          </fieldset>
+
+          <fieldset className="Fieldset">
+            <label className="Label" htmlFor="newPassword">
+              New password
+            </label>
+            <input className="Input" id="newPassword" type="password" />
+          </fieldset>
+
+          <fieldset className="Fieldset">
+            <label className="Label" htmlFor="confirmPassword">
+              Confirm password
+            </label>
+            <input className="Input" id="confirmPassword" type="password" />
+          </fieldset>
+
+          <div
+            style={{
+              display: "flex",
+              marginTop: 20,
+              justifyContent: "flex-end",
+            }}
+          >
+            <button className="Button green">Change password</button>
+          </div>
+        </Tabs.Content>
+      </Tabs.Root>
     </div>
   );
 }
