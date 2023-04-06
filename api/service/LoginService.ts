93cd250ec51aa0e7d1f1be67de08b555bbfc3933
Luana Lencina <analulencina@hotmail.com>
2023-04-06 15:42:05 -0300

    :rocket: feat(login): implementa login

---
+++ api/service/LoginService.ts
@@ -0,0 +1,17 @@
+import { get, post, put } from "@/api/Request";
+import { AxiosResponse } from "axios";
+//import { IFormUserPatient } from "@/utils/IFormUserPatient";
+
+const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
+
+export async function login(email: string, password: string): Promise<any> {
+  try {
+    const response = await post<AxiosResponse<any>>(`${API_ENDPOINT}/login/`, {
+      email,
+      password,
+    });
+    return { data: response.data.data, status: 1 };
+  } catch (error) {
+    return { data: {}, status: 0 };
+  }
+}
